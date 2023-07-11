eb22eee8e0c55dddf3b32571c1a29f0f65c8b929
DoubleJD <jd009@naver.com>
2023-07-11 23:12:50 +0900

    update

--- lib/client.ts
+++ lib/client.ts
@@ -1,11 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
 // eslint-disable-next-line import/no-anonymous-default-export
-const client = new PrismaClient();
-
-client.user.create({
-  data: {
-    email: "jd@test.com",
-    name: "jd",
-  },
-});
+export default new PrismaClient();
--- pages/api/client-test.tsx
+++ pages/api/client-test.tsx
@@ -1,8 +1,17 @@
+import client from "@/lib/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  await client.user.create({
+    data: {
+      email: "hi",
+      name: "hi",
+    },
+  });
   res.json({
     ok: true,
-    data: "xx",
   });
 }
